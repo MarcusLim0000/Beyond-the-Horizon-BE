c2fea73176af66338acf62c919f8154eaf098ac7
Marcus Lim <marcus.lwc7@gmail.com>
2024-06-08 14:20:36 +0800

    model updates

--- models/flight.model.js
+++ models/flight.model.js
@@ -24,7 +24,12 @@ const flightSchema = mongoose.Schema({
     cost: {
         type: Number,
         required: true
-    }
+    },
+    createdBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+      },
 }, {
     timestamps: true
 });
