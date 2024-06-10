c124e0ad8a6d26d2ce5a403840d587989a56e15b
Marcus Lim <marcus.lwc7@gmail.com>
2024-06-10 22:53:02 +0800

    added routes for getting flights and deletion

--- controllers/flightForm.controller.js
+++ controllers/flightForm.controller.js
@@ -11,6 +11,30 @@ async function createFlight(req, res) {
       return res.status(500).json({ error: error.message });
     }
   }
+
+  async function getFlights(req, res) {
+    try {
+      const flights = await Flight.find({createdBy: req.user._id});
+      return res.status(200).json(flights);
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({ error: error.message });
+    }
+  }
+
+  async function deleteListing(req, res) {
+    try {
+      const { id } = req.params;
+    const deletedListing = await Flight.findByIdAndDelete(id);
+    if (!deletedListing) {
+      return res.status(404).json({ error: 'Listing not found' });
+    }
+    res.json({ message: 'Listing deleted successfully' });
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({ error: error.message });
+    }
+  }
   
 //   async function getUserListings(req, res) {
 //     try {
@@ -74,4 +98,4 @@ async function createFlight(req, res) {
 //     }
 //   }
 
-  module.exports = { createFlight };
+  module.exports = { createFlight, getFlights, deleteListing };
--- routes/flightForm.routes.js
+++ routes/flightForm.routes.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const flightCtrl = require("../controllers/flightForm.controller");
-const hotelCtrl = require("../controllers/hotelForm.controller")
 
 router.post("/create", flightCtrl.createFlight);  
-//http://localhost:3000/api/listing/create
+//http://localhost:3000/api/flight/create
 
-router.post("/create", hotelCtrl.createHotel);  
-//http://localhost:3000/api/listing/create
+router.get('/:id', flightCtrl.getFlights);
+//http://localhost:3000/api/flight/:id
 
-
+router.delete('/delete/:id', flightCtrl.deleteListing);
+//http://localhost:3005/api/flight/delete/id
 
 module.exports = router;
