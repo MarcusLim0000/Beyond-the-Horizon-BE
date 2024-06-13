1df0c5c557a1b41c61c8da2c4e592f9ac55fe83d
Marcus Lim <marcus.lwc7@gmail.com>
2024-06-13 21:14:40 +0800

    added all functions to get all forms

--- controllers/hotelForm.controller.js
+++ controllers/hotelForm.controller.js
@@ -1,15 +1,32 @@
-const Hotel = require('../models/hotel.model')
+const Hotel = require("../models/hotel.model");
 
 async function createHotel(req, res) {
-    try {
-      const hotel = await Hotel.create({
-        ...req.body,
-        createdBy: req.user._id});
-      return res.status(200).json(hotel);
-    } catch (error) {
-      console.error(error);
-      return res.status(500).json({ error: error.message });
-    }
+  try {
+    const hotel = await Hotel.create({
+      ...req.body,
+      createdBy: req.user._id,
+    });
+    return res.status(200).json(hotel);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: error.message });
+  }
+}
+
+async function getHotel(req, res) {
+  const { id: holidayId } = req.params;
+
+  if (!holidayId) {
+    return res.status(400).json({ error: "Holiday ID is required" });
   }
 
-  module.exports = { createHotel };
+  try {
+    const hotels = await Hotel.find({ holidayId });
+    return res.status(200).json(hotels);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: error.message });
+  }
+}
+
+module.exports = { createHotel, getHotel };
--- routes/eventForm.routes.js
+++ routes/eventForm.routes.js
@@ -3,7 +3,10 @@ const router = express.Router();
 const eventCtrl = require("../controllers/eventForm.controller")
 
 router.post("/create", eventCtrl.createEvent);  
-//http://localhost:3000/api/listing/create
+//http://localhost:3000/api/event/create
+
+router.get('/:id', eventCtrl.getEvent);
+//http://localhost:3000/api/event/:id
 
 
 
