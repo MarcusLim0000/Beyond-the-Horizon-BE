a6e7674f88f78c3cdfb77760658d8b0627948598
Marcus Lim <marcus.lwc7@gmail.com>
2024-06-19 10:36:11 +0800

    refactored and added delete images functionality

--- models/images.model.js
+++ models/images.model.js
@@ -5,6 +5,7 @@ const imageSchema = new Schema(
   {
     url: { type: String, required: true },
     holidayId: { type: Schema.Types.ObjectId, ref: "Holiday", required: true },
+    public_id: { type: String, required: true },
   },
   { timestamps: true }
 );
