e7f9c8d56a4993fee397117e9d73dd01b16fd4dc
Marcus Lim <marcus.lwc7@gmail.com>
2024-06-23 09:24:14 +0800

    opps

--- server.js
+++ server.js
@@ -8,7 +8,8 @@ app.use(cors());
 // app.use(cors({
 //   origin: "https://beyond-the-horizon-fe.vercel.app/",
 // }));
-// app.use(express.json());
+
+app.use(express.json());
 
 app.use(require("./config/checkToken"));
 
